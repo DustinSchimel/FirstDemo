ea7d71d589a7b6a29b4b7b253e2a0f51956ac5a7
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-06 13:47:57 -0600

    Added settings and controller folders for sorting files

--- FirstDemo/ViewController.swift
+++ FirstDemo/ViewController.swift
@@ -12,6 +12,30 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var firstButton: UIButton!
     
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if ( firstButton.backgroundColor == .orange )
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
